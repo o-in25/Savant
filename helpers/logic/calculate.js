8e878caff4d9971b127d1ab300386cadfc66f161
Eoin Halligan <ehalligan12@gmail.com>
2018-05-03 15:08:41 -0400

    Version 2.1

--- helpers/logic/calculate.js
+++ helpers/logic/calculate.js
@@ -1,6 +1,9 @@
 // will take the reading of the sensor and will
 // produce a meaningful and desired result
 // implements moments.js
-var result = function(count) {
+var result = function(obj) {
     // TODO IMPLEMENT SOME ALGORITHM
+    var obj = JSON.parse(obj);
+
+
 };
--- models/service/insert.js
+++ models/service/insert.js
@@ -4,34 +4,19 @@ var util = require('./db.js');
 
 // returns the counter_data collection
 module.exports = {
-    // get the collection counter_table
-    getCounterDataCollection: function() {
-        // create tge promise
-        return new Promise(function(resolve, reject) {
-            util.connect(function() {
-                // get the dbobj
-                var dbobj = util.dbobj();
-                // find the collection 'counter data'
-                dbobj.db('info').collection('counter_data').find({}).toArray(function(err, res) {
-                    if(err) {
-                        console.log('The collection could not be found...');
-                        // failed
-                        reject();
-                    } else {
-                        // success
-                        resolve(res);
-                    }
-                });
-            })
-        }).then(function(collection) {
-            return new Promise(function(resolve, reject) {
-                if(typeof collection != 'undefined') {
-                    // return the value 'counter'
-                    resolve(collection[0].counter);
+    insertData: function (data) {
+        util.connect(function () {
+            // get the dbobj
+            var dbobj = util.dbobj();
+            // find the collection 'counter data'
+            dbobj.db('info').collection('users').insertOne(data, function (err, res) {
+                if (err) {
+                    console.log(err);
                 } else {
-                    reject();
+                    console.log(res);
                 }
-            });
+            })
         });
     }
 };
+
